2842409f1a3c3297dfcd16d20814398b130d0a8d
Winton <ios@churinc.com>
2020-06-19 18:07:55 +1000

    Updated error messages.

--- TestDrivenArchitecture/Enums/Errors.swift
+++ TestDrivenArchitecture/Enums/Errors.swift
@@ -13,7 +13,7 @@ enum Errors {
         static let emailEmpty = "Email address cannot be empty."
         static let emailInvalid = "Email format is incorrect."
         static let passwordEmpty = "The Password field cannot be empty."
-        static let passwordLengthInvalid = "Your password must be at least 8 characters. "
+        static let passwordLengthInvalid = "Your password must be at least 8 characters and less than 16 characters. "
         static let passwordConfirmEmpty = "Re-enter password field cannot be empty."
         static let passwordNotMatch = "The password you input is inconsistent."
     }
